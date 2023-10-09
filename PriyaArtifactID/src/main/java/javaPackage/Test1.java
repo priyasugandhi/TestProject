67b219295e56282777463736e27ce45930750532
Admin <Admin@DESKTOP-Q1DR8GR>
2023-10-09 04:34:57 -0700

    Add Test Project

---
+++ PriyaArtifactID/src/main/java/javaPackage/Test1.java
@@ -0,0 +1,17 @@
+package javaPackage;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test
+	public void b() {
+		System.out.println("Hello TestNG");
+	}
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello main method");
+
+	}
+}
